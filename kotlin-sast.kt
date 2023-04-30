8d6956b08d140bdbe0a5345f56e2527d975a9132
Liron <129161216+LironJit@users.noreply.github.com>
2023-04-30 16:47:13 +0300

    Update kotlin-sast.kt

--- kotlin-sast.kt
+++ kotlin-sast.kt
@@ -3,3 +3,14 @@
 fun main(args : Array<String>) {
     println("Hello, World!")
 }
+
+
+// x4 vulnerabilities
+
+val sql = "INSERT INTO user (username, password) VALUES ('$username', '$password');"
+
+var qry:String="INSERT INTO users (username, password) VALUES('"+username.text.toString()+"','"+password.text.toString()+"')";
+
+var qry:String="SELECT * FROM users WHERE username='"+username.text.toString()+"'";
+
+val cipher = Cipher.getInstance("TripleDES/CBC/PKCS5Padding")
