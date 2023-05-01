ed8f1c8c12e4178159c0fd2d413785a14209523e
Liron <129161216+LironJit@users.noreply.github.com>
2023-05-01 15:07:22 +0300

    Update python-test.py

--- python-test.py
+++ python-test.py
@@ -1,1 +1,23 @@
-# blank file :)
+# gitlab.bandit.B323 
+# Python.Lang.Security.Unverified-Ssl-Context.Unverified-Ssl-Context
+import ssl
+
+context = ssl._create_unverified_context()
+
+import ssl
+
+context = ssl._create_unverified_context()
+context = ssl._create_unverified_context()
+
+import ssl
+
+def create_context():
+    return ssl._create_unverified_context()
+
+context1 = create_context()
+context2 = create_context()
+
+import ssl
+
+context = ssl._create_unverified_context()
+response = requests.get("https://example.com", verify=context)
