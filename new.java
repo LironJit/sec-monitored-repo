a6f60e1c77e79fb7f31a28c0a917eeda97b42785
Liron <129161216+LironJit@users.noreply.github.com>
2023-05-11 11:22:45 +0300

    Create new.java

---
+++ new.java
@@ -0,0 +1,8 @@
+
+
+DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+factory.setFeature("http://xml.org/sax/features/external-general-entities", true);
+
+
+DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+factory.setFeature("http://xml.org/sax/features/external-parameter-entities", true);
