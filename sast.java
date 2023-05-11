9324f3882aba19ccf28c2877dbeb0e566ebe3880
Liron <129161216+LironJit@users.noreply.github.com>
2023-05-11 10:59:47 +0300

    Update sast.java

--- sast.java
+++ sast.java
@@ -1,1 +1,8 @@
-// hi
+import javax.xml.parsers.DocumentBuilderFactory;
+
+public class XMLParser {
+    public static void main(String[] args) {
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        factory.setFeature("http://xml.org/sax/features/external-general-entities", true);
+    }
+}
