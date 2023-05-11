31b57caadcc039751e86b409cab1bf371f665dc2
Liron <129161216+LironJit@users.noreply.github.com>
2023-05-11 11:13:34 +0300

    Update sast.java

--- sast.java
+++ sast.java
@@ -1,1 +1,8 @@
 // hi
+
+
+DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+factory.setFeature("http://xml.org/sax/features/external-general-entities", true);
+
+DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+factory.setFeature("http://xml.org/sax/features/external-parameter-entities", true);
