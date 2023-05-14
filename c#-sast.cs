f1abec2ded1e78a79721e6d607806d7c87c0c257
Liron <129161216+LironJit@users.noreply.github.com>
2023-05-14 11:14:04 +0300

    Update c#-sast.cs

--- c#-sast.cs
+++ c#-sast.cs
@@ -1,1 +1,48 @@
-// hi
+        public string BlindWithHttpClient(string path)
+        {
+            string result = "Message was sended";
+
+            HttpClient client = new HttpClient();
+
+            try
+            {
+                HttpResponseMessage response = client.GetAsync(path).Result;
+                var content = response.Content.ReadAsStringAsync().Result;
+                client.Dispose();
+            }
+            catch (Exception e)
+            {
+                System.Diagnostics.Debug.WriteLine(e);
+                result = "An error occured";
+            }
+
+            return result;
+        }
+        
+        
+        public string UnionBasedWithFormatString(string param)
+        {
+            string result = "";
+            string query = String.Format("SELECT * from [dbo].[USER] WHERE NAME = '{0}';", param);
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(
+                   GetConnectionString()))
+                    {
+                        connection.Open();
+
+                        SqlCommand command = new SqlCommand(query, connection);
+                        SqlDataReader reader = command.ExecuteReader();
+                        while (reader.Read())
+                        {
+                            result += String.Format("Username: {0} Role: {1}", reader["NAME"], reader["ROLE"]);
+                        }
+                    }
+            } catch(Exception e)
+            {
+                result = e.Message;
+            }
+
+            return result;
+        }
